ddc0e49924e0bd962df0435f300931c21c24012a
Raghu Betina <raghu.betina@chicagobooth.edu>
2023-06-08 13:53:55 +0000

    Display associated collections on show pages

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -42,8 +42,14 @@ class FollowsController < ApplicationController
   # DELETE /follows/1
   def destroy
     @follow.destroy
-    redirect_to follows_url, notice: 'Follow was successfully destroyed.'
+    message = "Follow was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to follows_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
