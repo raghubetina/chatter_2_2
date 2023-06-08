80ef09acf91a34cf21ffb935264251174700a4ca
Raghu Betina <raghu.betina@chicagobooth.edu>
2023-06-08 13:53:31 +0000

    Direct association between posts and likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,9 @@
 class Like < ApplicationRecord
   # Direct associations
 
+  belongs_to :post,
+             :counter_cache => true
+
   belongs_to :fan,
              :class_name => "User",
              :counter_cache => true
