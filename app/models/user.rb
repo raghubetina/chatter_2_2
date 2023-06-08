ee381c8f16322d34209b385ddebf5042fcdba654
Raghu Betina <raghu.betina@chicagobooth.edu>
2023-06-08 13:53:26 +0000

    Direct association between users and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :foreign_key => "commenter_id",
+             :dependent => :destroy
+
   has_many   :own_posts,
              :class_name => "Post",
              :foreign_key => "author_id",
