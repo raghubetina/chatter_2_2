54d4423fbd0415ea3aed5f2154299241445d483c
Raghu Betina <raghu.betina@chicagobooth.edu>
2023-06-08 13:53:42 +0000

    Whitelist columns in Like for ActiveAdmin

--- app/admin/likes.rb
+++ app/admin/likes.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Like do
 
+ permit_params :fan_id, :post_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
