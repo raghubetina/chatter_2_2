788b192710fe4ac5325bf02a01e15a365401de8b
Raghu Betina <raghu.betina@chicagobooth.edu>
2023-06-08 13:53:16 +0000

    Generate follows resource with ideas:scaffold

---
+++ app/models/follow.rb
@@ -0,0 +1,14 @@
+class Follow < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    leader.to_s
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :follows
   resources :likes
   resources :bookmarks
   resources :posts
