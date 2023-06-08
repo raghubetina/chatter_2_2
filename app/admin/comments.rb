48557705ddf6f3ba7d71e01af315117cd1d1c4f1
Raghu Betina <raghu.betina@chicagobooth.edu>
2023-06-08 13:53:44 +0000

    Whitelist columns in Comment for ActiveAdmin

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Comment do
 
+ permit_params :commenter_id, :post_id, :body
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
